dffe8721c8e7cf68ad5f643cadabb822e07d87f3
Michel Dufour <github@michel-dufour.fr>
2023-10-24 01:37:52 +0200

    MAJ la login_page

--- lib/presentation/pages/login_page.dart
+++ lib/presentation/pages/login_page.dart
@@ -1,11 +1,17 @@
-import 'package:project_firebase/presentation/pages/home_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_firebase/blocks/authentication_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:project_firebase/presentation/pages/register_page.dart';
+import 'package:project_firebase/presentation/widgets/auth_form.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
 
   static const String name = 'login';
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -14,16 +20,33 @@ class LoginPage extends StatelessWidget {
         title: const Text('Login Page'),
       ),
       body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: ElevatedButton(
-              onPressed: () {
-                context.goNamed(HomePage.name);
-              },
-              child: const Text('Se connecter'),
-            ),
+          AuthForm(
+            emailController: _emailController,
+            passwordController: _passwordController,
           ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // J'appelle le cubit pour me connecter.
+              context.read<AuthenticationCubit>().login(
+                    email: _emailController.text.trim(),
+                    password: _passwordController.text,
+                  );
+              // ATTENTION: On ne s'occupe pas de go_router car il sera
+              // lui-meme réactif aux changements d'états de connexion.
+            },
+            child: const Text('Se connecter'),
+          ),
+          const SizedBox(height: 10,),
+          TextButton(onPressed: () {
+            // rediriger vers la RegisterPage
+            context.goNamed(RegisterPage.name);
+          },
+          child: const Text('Aller à la page inscription'))
         ],
       ),
     );
