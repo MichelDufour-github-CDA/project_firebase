6cc6eb3a4a56e74b543bf3fff3f6909fa17c5c6c
Michel Dufour <github@michel-dufour.fr>
2023-10-24 17:18:57 +0200

    Ajoute le Service Student

---
+++ lib/services/student_service.dart
@@ -0,0 +1,9 @@
+import 'package:project_firebase/data_sources/student_data_source.dart';
+import 'package:project_firebase/models/student_model.dart';
+
+abstract class StudentService {
+  static Stream<List<StudentModel>> getStudents() =>
+      StudentDataSource.getStudents().map(
+        (data) => data.map((json) => StudentModel.fromJson(json)).toList(),
+      );
+}
