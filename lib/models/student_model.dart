1b479b87e1f2aaf0aacd710c229d6f2ca71e8b9c
Michel Dufour <github@michel-dufour.fr>
2023-10-24 16:42:05 +0200

    MAJ Model Student: ajout des méthodes fromJson & toJson

--- lib/models/student_model.dart
+++ lib/models/student_model.dart
@@ -8,4 +8,16 @@ class StudentModel {
     required this.lastName,
     required this.favoriteLanguage,
   });
+
+  factory StudentModel.fromJson(Map<String, dynamic> json) => StudentModel(
+        firstName: json['firstName']?.toString(),
+        lastName: json['lastName']?.toString(),
+        favoriteLanguage: json['favoriteLanguage']?.toString(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'firstName': firstName,
+        'lastName': lastName,
+        'favoriteLanguage': favoriteLanguage,
+      };
 }
