eeddda18c31b674f0ba35d1be6c20efeda650c12
Michel Dufour <github@michel-dufour.fr>
2023-10-24 16:38:33 +0200

    Ajoute le Model Student

---
+++ lib/models/student_model.dart
@@ -0,0 +1,11 @@
+class StudentModel {
+  final String? firstName;
+  final String? lastName;
+  final String? favoriteLanguage;
+
+  StudentModel({
+    required this.firstName,
+    required this.lastName,
+    required this.favoriteLanguage,
+  });
+}
